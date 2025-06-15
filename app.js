cd9063f6366e0616660772aff2a37cfb26ed0ec9
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-06-14 19:03:39 -0600

    add calculator constructor with first and second numbers of operations, results, operand and methods like add, divide, multipoly, reset, getResults

--- app.js
+++ app.js
@@ -14,3 +14,37 @@ function createElement(element, className) {
   newElement.classList.add(className);
   return newElement;
 }
+
+function Calculator() {
+  return {
+    firstNumber: 4,
+    secondNumber: 9,
+    result: 0,
+    currentOperation: null,
+    add: function () {
+      this.result = this.firstNumber + this.secondNumber;
+    },
+    sub: function () {
+      return this.firstNumber - this.secondNumber;
+    },
+    multiply: function () {
+      return this.firstNumber * this.secondNumber;
+    },
+    divide: function () {
+      return this.firstNumber / this.secondNumber;
+    },
+    reset: function () {
+      this.firstNumber = 0;
+      this.secondNumber = 0;
+      this.currentOperation = null;
+    },
+    getResult: function () {
+      return this.result;
+    },
+  };
+}
+
+const calculator = new Calculator();
+
+calculator.add();
+console.log(calculator.getResult());
