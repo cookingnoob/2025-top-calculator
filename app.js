777539a2b263fea0cb454ff68fb727988857744f
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-06-14 18:32:47 -0600

    add wrapper for the whole app, add styles for the color and size of calculator, screen and centered them

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const body = document.querySelector("body");
 
+const appWrapper = createElement("div", "app-wrapper");
 const calculatorWrapper = createElement("div", "calculator-wrapper");
 const screenWrapper = createElement("div", "screen-wrapper");
 
 calculatorWrapper.append(screenWrapper);
+appWrapper.append(calculatorWrapper);
 
-body.append(calculatorWrapper);
+body.append(appWrapper);
 
 function createElement(element, className) {
   const newElement = document.createElement(element);
